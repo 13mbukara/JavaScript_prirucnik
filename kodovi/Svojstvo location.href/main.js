bcbb39aa8ebd640c0034527fbe10136ef427d0eb
13mbukara <61122914+13mbukara@users.noreply.github.com>
2023-06-23 23:08:56 +0200

    Create main.js

---
+++ kodovi/Svojstvo location.href/main.js
@@ -0,0 +1,7 @@
+document.write("Href: " + location.href);
+document.write("</br>Protocol: " + location.protocol);
+document.write("</br>Host: " + location.host);
+document.write("</br>Hostname: " + location.hostname);
+document.write("</br>Port: " + location.port);
+document.write("</br>Path: " + location.path);
+document.write("</br>Pathname: " + location.pathname);
