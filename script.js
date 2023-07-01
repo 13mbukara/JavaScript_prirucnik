a63a19248c401902e717bfb43de456539eb7141c
13mbukara <61122914+13mbukara@users.noreply.github.com>
2023-07-01 11:40:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ btn.onclick = function () {
 
 var btn2 = document.getElementById("btn2");
 btn2.onfocus = function () {
-    btn2.style.backgroundColor = "red";
+  btn2.style.backgroundColor = "red";
 };
 
 var student = document.getElementById("student");
@@ -36,115 +36,26 @@ input.onselect = function () {
   alert("Označili ste tekst: " + input.value);
 };
 
-// GRANICA DOBROG I LOŠEG
+var listaInformacija = [];
 
-var select = document.getElementById("predmeti");
-select.onchange = function () {
-  alert("Odabrali ste predmet: " + select.value);
-};
-
-var form = document.getElementById("forma");
-form.onsubmit = function () {
-  alert("Forma je poslata!");
-};
-
-targetElement.addEventListener("scroll", (event) => {
-  // handle the scroll event
-});
-
-function evidencijaSaZakasnjenjem() {
-  console.log("Evidentirani ste!");
-}
-setTimeout(evidencijaSaZakasnjenjem, 3000); // Evidencija će biti ispisana nakon 3 sekunde (3000 milisekundi).
-
-function odbrojavanjeDoRezultata() {
-  console.log("Još jedna sekunda je prošla.");
-}
-setInterval(odbrojavanjeDoRezultata, 1000); // Odbrojavanje će se ispisivati svake sekunde.
-
-function pretraziStudenta() {
-  console.log("Pretraga izvršena.");
-}
-function debouncing(fn, delay) {
-  let timeoutId;
-
-  return function () {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(fn, delay);
-  };
-}
-const pretraziDebounced = debouncing(pretraziStudenta, 300); // Pretraga će se pokrenuti 300ms nakon poslednjeg poziva.
-
-/*
-function changeColor() {
-  var heading = document.querySelector("h1");
-  heading.style.color = "GREEN";
-}
-
-function revertColor() {
-  var heading = document.querySelector("h1");
-  heading.style.color = "blue";
-}
-
-var button = document.getElementById("btn");
-button.onmouseover = changeColor;
-button.onmouseout = revertColor;
-
-button.onclick = function () {
-  alert("Kliknuli ste dugme!");
-};
-
-var inputField = document.createElement("input");
-inputField.onfocus = function () {
-  console.log("Input polje je fokusirano.");
-};
-
-inputField.onblur = function () {
-  console.log("Input polje je izgubilo fokus.");
-};
-
-document.body.appendChild(inputField);
-
-inputField.onchange = function () {
-  console.log("Vrednost input polja se promenila.");
-};
-
-inputField.onselect = function () {
-  console.log("Tekst u input polju je selektovan.");
-};
-
-var form = document.createElement("form");
-form.onsubmit = function (event) {
+document.querySelector("form").addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log("Forma je submitovana.");
-};
 
-document.body.appendChild(form);
+  var studentName = document.getElementById("student-name").value;
+  var subject = document.getElementById("subject").value;
+  var grade = document.getElementById("grade").value;
 
-button.addEventListener("click", function () {
-  console.log("Kliknuli ste dugme (addEventListener).");
-});
-
-setTimeout(function () {
-  console.log("Ovo će se prikazati posle 3 sekunde.");
-}, 3000);
-
-var interval = setInterval(function () {
-  console.log("Ovo će se prikazivati svake 2 sekunde.");
-}, 2000);
-
-function debounce(func, delay) {
-  var timeoutId;
-
-  return function () {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(func, delay);
+  var informacija = {
+    studentName: studentName,
+    subject: subject,
+    grade: grade,
   };
-}
 
-var debouncedFunc = debounce(function () {
-  console.log("Ovo će se prikazivati nakon završetka unosa u input polje.");
-}, 500);
+  listaInformacija.push(informacija);
 
-inputField.addEventListener("input", debouncedFunc);
-*/
+  document.getElementById("student-name").value = "";
+  document.getElementById("subject").value = "";
+  document.getElementById("grade").value = "";
+
+  console.log(listaInformacija);
+});
